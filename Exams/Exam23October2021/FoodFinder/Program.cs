87e28cbf12fe33c1032b82dd49c6aad990ffa3e4
Pavlin Raykov <pavlin1984@abv.bg>
2022-02-01 14:07:04 +0200

    Add exam

---
+++ Exams/Exam23October2021/FoodFinder/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FoodFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var vowelsQueue = new Queue<char>(Console.ReadLine().Split(" ").Select(char.Parse).ToArray());
+            var consonantStack = new Stack<char>(Console.ReadLine().Split(" ").Select(char.Parse).ToArray());
+            
+
+            var listOfPossibleWords = new Dictionary<string, HashSet<char>>()
+            {
+                {"pear",new HashSet<char>()},
+                {"flour",new HashSet<char>()},
+                {"pork", new HashSet<char>()},
+                {"olive",new HashSet<char>()}
+            };
+            while (consonantStack.Count!=0)
+            {
+                
+                var currenConsonant = consonantStack.Pop();
+                var currentVowel = vowelsQueue.Dequeue();
+
+                foreach (var word in listOfPossibleWords.Keys)
+                {
+                    if (word.Contains(currentVowel))
+                    {
+                        listOfPossibleWords[word].Add(currentVowel);
+                    }
+                    if (word.Contains(currenConsonant))
+                    {
+                        listOfPossibleWords[word].Add(currenConsonant);
+                    }
+                }
+                vowelsQueue.Enqueue(currentVowel);
+                
+            }
+            int foundedWords = listOfPossibleWords.Where(x => x.Key.Length == x.Value.Count).Count();
+
+            Console.WriteLine($"Words found: {foundedWords}");
+
+            foreach (var word in listOfPossibleWords)
+            {
+                if (word.Key.Length==word.Value.Count)
+                {
+                    Console.WriteLine(word.Key);
+                }
+            }
+        }
+    }
+}
---
+++ Exams/Exam23October2021/PawnWars/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Linq;
+
+namespace PawnWars
+{
+    class Program
+    {
+        static char[,] chessboard;
+        static void Main(string[] args)
+        {
+
+            chessboard = new char[8, 8];
+            for (int row = 0; row < chessboard.GetLength(0); row++)
+            {
+                string rows = Console.ReadLine();
+
+                for (int col = 0; col < chessboard.GetLength(1); col++)
+                {
+                    chessboard[row, col] = rows[col];
+                }
+            }
+            int wRow = 0;
+            int wCol = 0;
+            int bRow = 0;
+            int bCol = 0;
+            for (int row = 0; row < 8; row++)
+            {
+                for (int col = 0; col < 8; col++)
+                {
+                    if (chessboard[row, col] == 'w')
+                    {
+                        wRow = row;
+                        wCol = col;
+                    }
+                    if (chessboard[row, col] == 'b')
+                    {
+                        bRow = row;
+                        bCol = col;
+                    }
+                }
+            }
+            while (true)
+            {
+                
+                if (IsInChesBoard(wRow - 1, wCol - 1, 8))
+                {
+                    if (chessboard[wRow - 1, wCol - 1] == 'b')
+                    {
+                        Console.WriteLine($"Game over! White capture on {(char)(bCol + 97)}{8 - bRow}.");
+                        break;
+                    }
+                }
+                if (IsInChesBoard(wRow - 1, wCol + 1, 8))
+                {
+                    if (chessboard[wRow - 1, wCol + 1] == 'b')
+                    {
+                        Console.WriteLine($"Game over! White capture on {(char)(bCol + 97)}{8 - bRow}.");
+                        break;
+                    }
+                }
+
+                if (IsInChesBoard(wRow - 1, wCol, 8))
+                {
+                    wRow -= 1;
+                   
+                    if (wRow == 0)
+                    {
+                        Console.WriteLine($"Game over! White pawn is promoted to a queen at {(char)(wCol + 97)}{8 - wRow}.");
+                        break;
+                    }
+                    chessboard[wRow, wCol] = 'w';
+
+                }
+                if (IsInChesBoard(bRow + 1, bCol - 1, 8))
+                {
+                    if (chessboard[bRow +1, bCol - 1] == 'w')
+                    {
+                        Console.WriteLine($"Game over! Black capture on {(char)(wCol + 97)}{8 - wRow}.");
+                        break;
+                    }
+                }
+                if (IsInChesBoard(bRow + 1, bCol + 1, 8))
+                {
+                    if (chessboard[bRow + 1, bCol + 1] == 'w')
+                    {
+                        Console.WriteLine($"Game over! Black capture on {(char)(wCol + 97)}{8 - wRow}.");
+                        break;
+                    }
+                }
+                if (IsInChesBoard(bRow + 1, bCol, 8))
+                {
+                    bRow += 1;
+                    if (bRow == 7)
+                    {
+                        Console.WriteLine($"Game over! Black pawn is promoted to a queen at {(char)(bCol + 97)}{8 - bRow}.");
+                        break;
+                    }
+                    chessboard[bRow, bCol] = 'b';
+
+                }
+            }
+
+           
+        }
+
+        private static bool IsInChesBoard(int row, int col, int v)
+        {
+            return row >= 0 && row < v && col >= 0 && col < v;
+        }     
+    }
+}
